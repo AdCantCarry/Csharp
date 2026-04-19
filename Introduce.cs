0cd5b83929c158372e345983cc9b35175005a39e
AdCantCarry <144407363+AdCantCarry@users.noreply.github.com>
2026-04-19 12:15:21 +0700

    nhập tên , tuổi và in ra màn hình

---
+++ Introduce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Introduce
+{
+    public static void Main(string[] args)
+    {
+
+        //string name = "Nguyen Anh Duy";
+
+        //int age = 22;
+
+        //bool laSinhVien = true;
+
+        //Console.WriteLine(name);
+
+        //Console.WriteLine(age);
+
+        //Console.WriteLine(laSinhVien);
+
+        Console.WriteLine("Nhap ten cua ban: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Nhap tuoi: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Xin chao " + name);
+        Console.WriteLine("Ban " + age + " tuoi");
+
+    }
+}
