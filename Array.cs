46ec7a29382208edf2dc7fbe439142a81bd6c2e0
AdCantCarry <144407363+AdCantCarry@users.noreply.github.com>
2026-04-27 13:36:25 +0700

    foreach_forloop

--- Array.cs
+++ Array.cs
@@ -56,25 +56,67 @@ namespace C__tutorial
 
 
 
-            int[] danhSachDiem = { 8, 4, 9 };
+            //int[] danhSachDiem = { 8, 4, 9 };
 
-            // Vòng lặp chạy từ 0 đến 2 (vì có 3 phần tử)
-            for (int i = 0; i < danhSachDiem.Length; i++)
-            {
-                // Lấy điểm tại vị trí i ra để kiểm tra
-                int diemHienTai = danhSachDiem[i];
+            //// Vòng lặp chạy từ 0 đến 2 (vì có 3 phần tử)
+            //for (int i = 0; i < danhSachDiem.Length; i++)
+            //{
+            //    // Lấy điểm tại vị trí i ra để kiểm tra
+            //    int diemHienTai = danhSachDiem[i];
 
-                Console.Write("Môn thứ " + i + " có điểm là " + diemHienTai + ": ");
+            //    Console.Write("Môn thứ " + i + " có điểm là " + diemHienTai + ": ");
 
-                if(diemHienTai < 5)
-                {
-                    Console.WriteLine("Phải thi lại");
-                }
-                else
-                {
-                    Console.WriteLine("Qua môn");
-                }
-            }
+            //    if(diemHienTai < 5)
+            //    {
+            //        Console.WriteLine("Phải thi lại");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Qua môn");
+            //    }
+            //}
+
+
+
+
+            //int[] danhSachDiem = { 4, 7, 9 };
+
+            //// Dùng vòng lặp for để "tặng" mỗi bạn 1 điểm
+            //for (int i = 0; i < danhSachDiem.Length; i++)
+            //{
+            //    // Truy cập trực tiếp vào ngăn thứ i và cộng thêm 1
+            //    danhSachDiem[i] = danhSachDiem[i] + 1;
+            //    int diemHienTai = danhSachDiem[i];
+
+            //    Console.Write("Môn thứ " + i + " có điểm là " + diemHienTai + ": ");
+
+            //    if (diemHienTai < 5)
+            //    {
+            //        Console.WriteLine("Phải thi lại");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Qua môn");
+            //    }
+            //}
+
+
+            //int[] danhSachDiem = { 8, 4, 9 };
+
+            //Console.WriteLine("--- KIỂM TRA KẾT QUẢ ---");
+
+            //// Với mỗi con "diem" nằm trong "danhSachDiem"
+            //foreach (int diem in danhSachDiem)
+            //{
+            //    if (diem < 5)
+            //    {
+            //        Console.WriteLine(diem + ": Phải thi lại");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(diem + ": Qua môn");
+            //    }
+            //}
         }   
     }
 }
